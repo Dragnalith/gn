84654e74c80fa551f538c58e33c1f97d98c93a16
Marc Delorme <delormemarco@gmail.com>
2020-02-24 15:22:23 +0900

    Generate a buildable .csproj

--- src/gn/ninja_csharp_assembly_target_writer.cc
+++ src/gn/ninja_csharp_assembly_target_writer.cc
@@ -15,8 +15,22 @@
 #include "gn/rust_substitution_type.h"
 #include "gn/substitution_writer.h"
 #include "gn/target.h"
+#include "gn/xml_element_writer.h"
 
 namespace {
+
+struct SourceFileWriter {
+  SourceFileWriter(PathOutput& path_output, const SourceFile& source_file)
+      : path_output_(path_output), source_file_(source_file) {}
+  ~SourceFileWriter() = default;
+
+  void operator()(std::ostream& out) const {
+    path_output_.WriteFile(out, source_file_);
+  }
+
+  PathOutput& path_output_;
+  const SourceFile& source_file_;
+};
 
 // Returns the proper escape options for writing compiler and linker flags.
 EscapeOptions GetFlagOptions() {
@@ -42,7 +56,8 @@ NinjaCSharpAssemblyTargetWriter::NinjaCSharpAssemblyTargetWriter(
     std::ostream& csproj_out)
     : NinjaBinaryTargetWriter(target, out),
       csproj_out_(csproj_out),
-      msbuild_tool_(target->toolchain()->GetToolForTargetFinalOutputAsCSharp(target)) {}
+      msbuild_tool_(
+          target->toolchain()->GetToolForTargetFinalOutputAsCSharp(target)) {}
 
 NinjaCSharpAssemblyTargetWriter::~NinjaCSharpAssemblyTargetWriter() = default;
 
@@ -56,8 +71,7 @@ void NinjaCSharpAssemblyTargetWriter::Run() {
 
   out_ << ": " << rule_prefix_ << msbuild_tool_->name();
   out_ << " ";
-  path_output_.WriteFile(out_,
-                         target_->csharp_values().project_path());
+  path_output_.WriteFile(out_, target_->csharp_values().project_path());
   out_ << " |";
   for (const auto& source : target_->sources()) {
     out_ << " ";
@@ -65,4 +79,91 @@ void NinjaCSharpAssemblyTargetWriter::Run() {
   }
 
   out_ << std::endl;
+
+  SourceDir project_dir = target_->csharp_values().project_path().GetDir();
+  SourceDir output_dir =
+      output_files[0]
+          .AsSourceFile(target_->settings()->build_settings())
+          .GetDir();
+  SourceDir target_dir = target_->label().dir();
+
+#if 0
+  csproj_out_ << "proj_dir: " << project_dir.value() << '\n';
+  csproj_out_ << "out_dir: " << output_dir.value() << '\n';
+  csproj_out_ << "target_dir" << target_dir.value() << '\n';
+  for (const auto& source : target_->sources()) {
+    csproj_out_ << "source: ";
+    path_output_.WriteFile(csproj_out_, source);
+    csproj_out_ << '\n';
+    csproj_out_ << "link: " << RebasePath(source.value(), target_dir) << '\n';
+  }
+  csproj_out_ << "output: " << output_files[0].value();
+#endif
+  PathOutput project_path_output(
+      GetBuildDirForTargetAsSourceDir(target_, BuildDirType::OBJ),
+      target_->settings()->build_settings()->root_path_utf8(),
+      EscapingMode::ESCAPE_NONE);
+
+  csproj_out_ << "<?xml version=\"1.0\" encoding=\"utf-8\"?>" << std::endl;
+  XmlElementWriter project(
+      csproj_out_, "Project",
+      XmlAttributes("ToolsVersion", "15.0")
+          .add("xmlns", "http://schemas.microsoft.com/developer/msbuild/2003"));
+
+  project.SubElement(
+      "Import", XmlAttributes("Project",
+                              "$(MSBuildExtensionsPath)\\$(MSBuildToolsVersion)"
+                              "\\Microsoft.Common.props")
+                    .add("Condition",
+                         "Exists('$(MSBuildExtensionsPath)\\$("
+                         "MSBuildToolsVersion)\\Microsoft.Common.props')"));
+
+  {
+    auto commonPropertyGroup = project.SubElement("PropertyGroup");
+    commonPropertyGroup->SubElement("Platform")->Text("AnyCPU");
+    commonPropertyGroup->SubElement("ProjectGuid")
+        ->Text("{696F6F3E-15C6-4DB8-ABE9-1FC7641E1B9F}");
+    commonPropertyGroup->SubElement("OutputType")->Text("Library");
+    commonPropertyGroup->SubElement("RootNamespace")->Text("AppName");
+    commonPropertyGroup->SubElement("AssemblyName")->Text("AppName");
+    commonPropertyGroup->SubElement("TargetFrameworkVersion")->Text("v4.6.1");
+    commonPropertyGroup->SubElement("FileAlignment")->Text("512");
+    commonPropertyGroup->SubElement("AutoGenerateBindingRedirects")
+        ->Text("true");
+  }
+  {
+    auto propertyGroup = project.SubElement("PropertyGroup");
+    propertyGroup->SubElement("PlatformTarget")->Text("AnyCPU");
+    propertyGroup->SubElement("DebugSymbols")->Text("true");
+    propertyGroup->SubElement("Optimize")->Text("false");
+    propertyGroup->SubElement("OutputPath")
+        ->Text(RebasePath(output_dir.value(), project_dir));
+    propertyGroup->SubElement("DefineConstants")->Text("DEBUG;TRACE");
+    propertyGroup->SubElement("ErrorReport")->Text("prompt");
+    propertyGroup->SubElement("WarningLevel")->Text("4");
+  }
+  {
+    auto itemGroup = project.SubElement("ItemGroup");
+    itemGroup->SubElement("Reference", XmlAttributes("Include", "System"));
+    itemGroup->SubElement("Reference", XmlAttributes("Include", "System.Core"));
+    itemGroup->SubElement("Reference", XmlAttributes("Include", "System.Xml.Linq"));
+    itemGroup->SubElement("Reference", XmlAttributes("Include", "System.Data.DataSetExtensions"));
+    itemGroup->SubElement("Reference", XmlAttributes("Include", "Microsoft.CSharp"));
+    itemGroup->SubElement("Reference", XmlAttributes("Include", "System.Data"));
+    itemGroup->SubElement("Reference", XmlAttributes("Include", "System.Net.Http"));
+    itemGroup->SubElement("Reference", XmlAttributes("Include", "System.Xml"));
+  }
+  {
+    auto itemGroup = project.SubElement("ItemGroup");
+    for (const auto& source : target_->sources()) {
+      itemGroup
+          ->SubElement("Compile", "Include",
+                       SourceFileWriter(project_path_output, source))
+          ->SubElement("Link")
+          ->Text(RebasePath(source.value(), target_dir));
+    }
+  }
+  project.SubElement(
+      "Import", XmlAttributes("Project",
+                              "$(MSBuildToolsPath)\\Microsoft.CSharp.targets"));
 }
